c4b9366b6615a4d6f17460eb3c32d44f94ca91a7
ruyaatatreh99 <ruyaatatreh99@gmail.com>
2023-08-05 16:33:29 +0300

    commit

---
+++ screens/home.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View >
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
