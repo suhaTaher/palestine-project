43b1add367cb473a18b0611d66fd70a9eb3d98aa
ruyaatatreh99 <ruyaatatreh99@gmail.com>
2023-08-05 23:15:49 +0300

    sign in

--- Components/FilledButton.js
+++ Components/FilledButton.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Button,Dimensions} from 'react-native';
+import { StyleSheet, View, Button,Text,Dimensions} from 'react-native';
 import React from 'react';
-import { Slide } from 'react-slideshow-image';
+
 //import 'react-slideshow-image/dist/styles.css'
 import Styling from '../Styling';
 
@@ -14,31 +14,30 @@ const Hight=Dimensions.get('window').height;
 
 
 export default function FilledButton({color, text , BWidth ,radius  }) { // recomended radious 45
-  const wid=eval(BWidth);
   return (
 
-    <View style={[styles.Button,{width:wid*Width,borderRadius:radius, backgroundColor:color}]}>
-        <Button color='white' title={text} ></Button>
+    <View style={[styles.Button,{ width:BWidth,backgroundColor:color,borderRadius:radius}]}>
+      <Text style={styles.Text}>{text}</Text>
+       
     </View>    
    
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-
-    },
+ 
     Button:{
-      padding:5,
-      position:'absolute'
-    
+      textAlign:'center',
+      position: 'relative',
+      height: 45,
+      margin: 15,
+      padding: 15,
+     
+    },
+    Text:{
+      color :'#fff',
+      textAlign:'center',
+     fontSize:13,
     }
 
-
   });
-
-
-
-
